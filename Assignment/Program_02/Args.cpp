c620ae5598b44804ac38a6a1d6a46f90032bec0d
Cmartinez-28 <cmartinez.8631@gmail.com>
2026-03-17 18:16:07 -0500

    UPDATE Args.cpp

--- Assignment/Program_02/Args.cpp
+++ Assignment/Program_02/Args.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #include<iostream>
 #include <cstring>
+#include <string>
 
     Args :: Args()
     {
@@ -9,11 +10,14 @@ using namespace std;
         blur = false;
         flipH = false;
         flipV = false;
+        brighten = false;
+        rotate = false;
     }
 
     void Args :: printSummary()
     {
         string flags = "" , delim = "";
+        string params = "";
         if (grayscale)
         {
             flags = "grayscale";
@@ -34,9 +38,22 @@ using namespace std;
             flags = flags + delim + "flipV";
             delim = " ";
         }
+        if (brighten)
+        {
+            val = stoi(brlevel);
+            params = params + delim + "brighten=" + to_string(val);
+            delim = " ";
+        }
+        if (rotate)
+        {
+            val = stoi(rotlevel);
+            params = params + delim + "rotate=" + to_string(val);
+            delim = " ";
+        }
         cout << "Input: " << input << endl;
         cout << "Output: " << output << endl;
         cout << "Flags: " << flags << endl; 
+        cout << "Params: " << params << endl;
     }
 
     bool Args::parse(int argc, char* argv[])
@@ -51,12 +68,11 @@ using namespace std;
             input = argv[1];
             output = argv[2];
             for (int i = 3; i < argc; i++)
-            {
+            {   //if statements for all flags
                 if (strcmp (argv[i],"--grayscale") == 0 || strcmp(argv[i], "-g") == 0)
                 {
                     grayscale = true;
                 }
-
                 if (strcmp (argv[i],"--blur") == 0 || strcmp(argv[i], "-l") == 0)
                 {
                     blur = true;
@@ -68,6 +84,41 @@ using namespace std;
                 if (strcmp (argv[i],"--flipV") == 0 || strcmp(argv[i], "-v") == 0)
                 {
                     flipV = true;
+                }
+                //logic for options with values
+                if (strcmp(argv[i], "--brighten") == 0 || strcmp(argv[i], "-b") == 0)
+                {
+                    if (i+1 < argc)
+                    {
+                        brighten = true;
+                        brlevel = argv[++i];
+                    }
+                    else 
+                    {
+                        cout << "Brighten requires another argument" << endl;
+                    }
+                }
+                if(strncmp(argv[i], "--brighten=", 11) ==0)
+                {
+                    brighten = true;
+                    brlevel = argv[i] + 11;
+                }
+                if (strcmp(argv[i], "--rotate") == 0 || strcmp(argv[i], "-r") == 0)
+                {
+                    if (i+1 < argc)
+                    {
+                        rotate = true;
+                        rotlevel = argv[++i];
+                    }
+                    else 
+                    {
+                        cout << "Rotate requires another argument" << endl;
+                    }
+                }
+                 if(strncmp(argv[i], "--rotate=", 9) ==0)
+                {
+                    rotate = true;
+                    rotlevel = argv[i] + 9;
                 }
             }
             result = true;
